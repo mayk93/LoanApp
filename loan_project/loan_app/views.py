c25bc1af63ba537b8b4c4f379442f4db03a461cf
michael <mihai@mandrescu.co>
2017-06-04 20:22:40 +0100

    Purely generic views

--- loan_project/loan_app/views.py
+++ loan_project/loan_app/views.py
@@ -1,35 +1,13 @@
 from models import Loan
 from serializers import LoanSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class LoanList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
-
+class LoanList(generics.ListCreateAPIView):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class LoanDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class LoanDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
