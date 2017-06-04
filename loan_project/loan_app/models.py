3849383767aea8274f158bab3223a1eb2f94f0fe
michael <mihai@mandrescu.co>
2017-06-04 19:48:39 +0100

    Basic project - Added loan model and serializer

--- loan_project/loan_app/models.py
+++ loan_project/loan_app/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+'''
+The Loan information is a simple high level view of the company requesting the Loan (ie its
+sector and a synopsis of what it does) and how much they are looking to borrow.
+'''
+
+
+class Loan(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    company = models.CharField(max_length=100)
+    sector = models.CharField(max_length=100)
+    synopsis = models.TextField(blank=True, default='')
+    amount = models.IntegerField(blank=False)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('created',)
