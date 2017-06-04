78927a7350065bd7b1a8e3e508911454891fdcfa
michael <mihai@mandrescu.co>
2017-06-04 20:14:17 +0100

    Api view

--- loan_project/loan_app/urls.py
+++ loan_project/loan_app/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
 import views
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^loans/$', views.loan_list),
     url(r'^loan/(?P<pk>[0-9]+)/$', views.loan_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- loan_project/loan_app/views.py
+++ loan_project/loan_app/views.py
@@ -1,54 +1,49 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from models import Loan
 from serializers import LoanSerializer
 
 
-@csrf_exempt
-def loan_list(request):
+@api_view(['GET', 'POST'])
+def loan_list(request, format=None):
     """
     List all loans, or create a new loan.
     """
     if request.method == 'GET':
-        snippets = Loan.objects.all()
-        serializer = LoanSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        loans = Loan.objects.all()
+        serializer = LoanSerializer(loans, many=True)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = LoanSerializer(data=data)
+        serializer = LoanSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def loan_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def loan_detail(request, pk, format=None):
     """
-    Retrieve, update or delete a loan.
+    Retrieve, update or delete a loan instance.
     """
     try:
-        snippet = Loan.objects.get(pk=pk)
+        loan = Loan.objects.get(pk=pk)
     except Loan.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = LoanSerializer(snippet)
-        return JsonResponse(serializer.data)
+        serializer = LoanSerializer(loan)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = LoanSerializer(snippet, data=data)
+        serializer = LoanSerializer(loan, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+        loan.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
