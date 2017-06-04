a6d5f0097e1ecf684623f2c4e4d9e1f3a131b779
michael <mihai@mandrescu.co>
2017-06-04 20:21:09 +0100

    Mixins and generics

--- loan_project/loan_app/views.py
+++ loan_project/loan_app/views.py
@@ -1,52 +1,35 @@
 from models import Loan
 from serializers import LoanSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 
-class LoanList(APIView):
-    """
-    List all loans, or create a new loan.
-    """
-    def get(self, request, format=None):
-        loans = Loan.objects.all()
-        serializer = LoanSerializer(loans, many=True)
-        return Response(serializer.data)
+class LoanList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
 
-    def post(self, request, format=None):
-        serializer = LoanSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class LoanDetail(APIView):
-    """
-    Retrieve, update or delete a loan instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Loan.objects.get(pk=pk)
-        except Loan.DoesNotExist:
-            raise Http404
+class LoanDetail(mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
 
-    def get(self, request, pk, format=None):
-        loan = self.get_object(pk)
-        serializer = LoanSerializer(loan)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        loan = self.get_object(pk)
-        serializer = LoanSerializer(loan, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        loan = self.get_object(pk)
-        loan.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
