5ee3a21d32f0167debc2a974d9d4bd1d4cea72bb
michael <mihai@mandrescu.co>
2017-06-04 21:48:52 +0100

    Url routing

--- loan_project/loan_app/urls.py
+++ loan_project/loan_app/urls.py
@@ -1,35 +1,12 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import renderers
+from django.conf.urls import url, include
+import views
+from rest_framework.routers import DefaultRouter
 
-from views import LoanViewSet, UserViewSet, api_root
+router = DefaultRouter()
+router.register(r'loans', views.LoanViewSet)
+router.register(r'users', views.UserViewSet)
 
-loan_list = LoanViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-loan_detail = LoanViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-loan_synopsis = LoanViewSet.as_view({
-    'get': 'synopsis'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-    url(r'^loans/$', loan_list, name='loan-list'),
-    url(r'^loans/(?P<pk>[0-9]+)/$', loan_detail, name='loan-detail'),
-    url(r'^loans/(?P<pk>[0-9]+)/synopsis/$', loan_synopsis, name='loan-synopsis'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
-])
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
