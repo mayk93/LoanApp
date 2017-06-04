f52f1f5a63ad53adf2e1401db99fa2d7effa1e99
michael <mihai@mandrescu.co>
2017-06-04 21:45:23 +0100

    Permissions fix and view set.

--- loan_project/loan_app/urls.py
+++ loan_project/loan_app/urls.py
@@ -1,29 +1,35 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-import views
+from rest_framework import renderers
 
-# API endpoints
+from views import LoanViewSet, UserViewSet, api_root
+
+loan_list = LoanViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+loan_detail = LoanViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+loan_synopsis = LoanViewSet.as_view({
+    'get': 'synopsis'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
 urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^loans/$',
-        views.LoanList.as_view(),
-        name='loan-list'),
-    url(r'^loans/(?P<pk>[0-9]+)/$',
-        views.LoanDetail.as_view(),
-        name='loan-detail'),
-    url(r'^loans/(?P<pk>[0-9]+)/synopsis/$',
-        views.LoanSynopsis.as_view(),
-        name='loan-synopsis'),
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail')
+    url(r'^$', api_root),
+    url(r'^loans/$', loan_list, name='loan-list'),
+    url(r'^loans/(?P<pk>[0-9]+)/$', loan_detail, name='loan-detail'),
+    url(r'^loans/(?P<pk>[0-9]+)/synopsis/$', loan_synopsis, name='loan-synopsis'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
 ])
-
-# Login and logout views for the browsable API
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
-]
--- loan_project/loan_app/views.py
+++ loan_project/loan_app/views.py
@@ -2,45 +2,29 @@ from django.contrib.auth.models import User
 from models import Loan
 from serializers import LoanSerializer, UserSerializer
 from permisions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions, renderers
+from rest_framework import permissions, renderers, viewsets
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-class LoanList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
+class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def synopsis(self, request, *args, **kwargs):
+        loan = self.get_object()
+        return Response(loan.synopsis)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class LoanDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-
-    queryset = Loan.objects.all()
-    serializer_class = LoanSerializer
-
-
-class LoanSynopsis(generics.GenericAPIView):
-    queryset = Loan.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        loan = self.get_object()
-        return Response(loan.synopsis)
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
